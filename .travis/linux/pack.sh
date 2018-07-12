5b81a6615232c69e543122c97230ca7ef40568f3
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2018-07-12 17:31:04 +0200

    Automatic upload of deb package - try 7

--- .travis/linux/pack.sh
+++ .travis/linux/pack.sh
@@ -1,6 +1,5 @@
 docker exec dd bash -c "cd build; cpack"
 docker exec dd bash -c "cd build; mv *.deb  ../BornAgain-dev-xenial.deb"
-docker exec dd bash -c "wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh"
-docker exec dd bash -c "bash upload.sh BornAgain*.deb*"
+docker cp dd:/source/build/BornAgain-dev-xenial.deb ./
 
 
