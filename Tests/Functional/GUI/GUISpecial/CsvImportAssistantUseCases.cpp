999bcb2ca119d8aa9582a084211a155750c766cd
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2018-12-03 18:02:57 +0100

    Beautify functional tests code

--- Tests/Functional/GUI/GUISpecial/CsvImportAssistantUseCases.cpp
+++ Tests/Functional/GUI/GUISpecial/CsvImportAssistantUseCases.cpp
@@ -2,8 +2,8 @@
 //
 //  BornAgain: simulate and fit scattering at grazing incidence
 //
-//! @file      Tests/Functional/GUI/GUIUseCases/GUIUseCases.cpp
-//! @brief     Implements GUI performance functional test.
+//! @file      Tests/Functional/GUI/GUISpecial/GUIUseCases.cpp
+//! @brief     Implements Import Assistant functional test.
 //!
 //! @homepage  http://www.bornagainproject.org
 //! @license   GNU General Public License v3 or higher (see COPYING)
@@ -35,61 +35,33 @@ std::string vectorToString(std::vector<double> X)
 {
     std::stringstream buffer;
     buffer << "{ ";
-    for(auto x : X)
+    for (auto x : X)
         buffer << x << ",";
     buffer << " }";
     return buffer.str();
 }
-template<class T>
-bool areVectorsEqual(std::vector<T> &X, std::vector<T> &Y){
-    if(X.size() != Y.size())
+template <class T> bool areVectorsEqual(std::vector<T>& X, std::vector<T>& Y)
+{
+    if (X.size() != Y.size())
         return false;
     size_t size = X.size();
-    for(size_t t = 0; t < size ; ++t)
-        if(fabs(X[t] - Y[t]) > 1e-14){
+    for (size_t t = 0; t < size; ++t)
+        if (fabs(X[t] - Y[t]) > 1e-14) {
             std::cout << Y[t] - X[t] << std::endl;
             return false;
         }
     return true;
-
 }
 }
 
-CsvImportAssistantUseCases::CsvImportAssistantUseCases()
-{
-    m_dataFileParameters = {
-        //{"filename", firstRow, lastRow, coordinateColumn, intensityColumn, expCoords, expIntens},
-//        {"probeA_merged.txt", m_firstRow, m_lastRow, m_coordinateColumn, m_intensityColumn, m_expCoords, m_expIntens},
-//        {"mg6c_Merged.txt", m_firstRow, m_lastRow, m_coordinateColumn, m_intensityColumn, m_expCoords, m_expIntens},
-//        {"raw_data_MG-6a-XRR-Standard_0-1_5deg-step1-2.csv", m_firstRow, m_lastRow, m_coordinateColumn, m_intensityColumn, m_expCoords, m_expIntens},
-//        {"mg6c_Merged.png", m_firstRow, m_lastRow, m_coordinateColumn, m_intensityColumn, m_expCoords, m_expIntens},
-//        {"Cut_2.dat", m_firstRow, m_lastRow, m_coordinateColumn+2, m_intensityColumn, m_expCoords, m_expIntens},
-//        {"Cut_1.dat", m_firstRow, m_lastRow, m_coordinateColumn+2, m_intensityColumn, m_expCoords, m_expIntens},
-//        {"p12537_00000125.dat", 200+m_firstRow, 200+m_lastRow, m_coordinateColumn, m_intensityColumn+5, m_expCoords, m_expIntens},
-//        {"p12537_00000128.dat", 200+m_firstRow, 200+m_lastRow, m_coordinateColumn, m_intensityColumn+5, m_expCoords, m_expIntens},
-//        {"p15145_00005044.dat", 200+m_firstRow, 200+m_lastRow, m_coordinateColumn, m_intensityColumn+9, m_expCoords, m_expIntens},
-//        {"p15145_00005023.dat", 200+m_firstRow, 200+m_lastRow, m_coordinateColumn, m_intensityColumn+13, m_expCoords, m_expIntens},
-//        {"NR_System2_D2O_T20.DAT", m_firstRow, m_lastRow, m_coordinateColumn, m_intensityColumn+1, m_expCoords, m_expIntens},
-//        {"R_d2o.txt", m_firstRow, m_lastRow, m_coordinateColumn, m_intensityColumn, m_expCoords, m_expIntens},
-//        {"P_ME_305_D2O_15C_01.DAT", m_firstRow, m_lastRow, m_coordinateColumn+2, m_intensityColumn-1, m_expCoords, m_expIntens},
-//        {"P_ME_3026_D2O_60C_05.DAT", m_firstRow, m_lastRow, m_coordinateColumn+2, m_intensityColumn-1, m_expCoords, m_expIntens},
-        {"b_104.dat", m_firstRow, m_lastRow, m_coordinateColumn, m_intensityColumn, m_expCoords, m_expIntens},
-    };
-}
-
-CsvImportAssistantUseCases::~CsvImportAssistantUseCases()
-{
-}
-
-// ---
 bool CsvImportAssistantUseCases::runTest()
 {
-    std::cout << "CsvImportAssistantPerformanceTest -> Running ..." << std::endl;
-    bool testPassed = true;
+    std::cout << "CsvImportAssistantUseCases -> Running ..." << std::endl;
 
     for (auto thisFile : m_dataFileParameters) {
+        std::cout << "----- File: " << thisFile.filename << " -----" << std::endl;
+
         const QString f = QString::fromStdString(path(thisFile.filename));
-        std::cout << f.toStdString() << std::endl;
         CsvImportAssistant assistant(f);
 
         assistant.setFirstRow(thisFile.firstRow);
@@ -98,13 +70,17 @@ bool CsvImportAssistantUseCases::runTest()
         assistant.setIntensityColumn(thisFile.intensityColumn, m_intensityMultiplier);
 
         ImportDataInfo currentData = assistant.getData();
-        if(!currentData){
+        if (!currentData) {
             std::cout << "Empty Data!" << std::endl;
+            if (thisFile.filename == "NonExistentFile") {
+                std::cout << "...it is ok in this case." << std::endl;
+                continue;
+            }
             return false;
         }
 
-        auto   iData_ptr = currentData.intensityData();
-        if(!iData_ptr){
+        auto iData_ptr = currentData.intensityData();
+        if (!iData_ptr) {
             std::cout << "No intensity data!" << std::endl;
             return false;
         }
@@ -113,52 +89,23 @@ bool CsvImportAssistantUseCases::runTest()
         size_t expectedAllocSize = size_t(1 + thisFile.lastRow - thisFile.firstRow);
 
         size_t DataRank = currentData.dataRank();
-        size_t expectedDataRank = 1; //Reflectivity data is 1d, so rank is 1;
+        size_t expectedDataRank = 1;
 
         auto CoordinateValues = iData_ptr->getAxis(0).getBinCenters();
         const std::vector<double> expectedCoordVals = thisFile.expectedCoordinateValues;
 
         auto IntensityValues = iData_ptr->getRawDataVector();
-        IntensityValues.shrink_to_fit();
         std::vector<double> expectedIntensVals = thisFile.expectedIntensityValues;
-        expectedIntensVals.shrink_to_fit();
 
         auto UnitsLabel = currentData.unitsLabel();
         auto expectedUnitsLabel = JobItemUtils::nameFromAxesUnits(m_units);
 
         auto AxisLabel0 = assistant.getData().axisLabel(0);
-        auto expectedAxisLabel0 =
-            QString::fromStdString(AxisNames::InitSpecAxis()[m_units]);
+        auto expectedAxisLabel0 = QString::fromStdString(AxisNames::InitSpecAxis()[m_units]);
 
         auto AxisLabel1 = assistant.getData().axisLabel(1);
         auto expectedAxisLabel1 = SpecularDataAxesNames::y_axis_default_name;
 
-
-        std::cout << "--------------------------------------------- " << std::endl;
-//        std::cout << thisFile.filename
-//                  << "\n"
-//                  << thisFile.firstRow
-//                  << "\n"
-//                  << thisFile.lastRow
-//                  << "\n"
-//                  << thisFile.coordinateColumn
-//                  << "\n"
-//                  << thisFile.intensityColumn
-//                  << "\n"
-//                  << vectorToString(CoordinateValues)
-//                  << "\n"
-//                  << vectorToString(IntensityValues)
-//                  << "\n"
-//                  << UnitsLabel.toStdString()
-//                  << "\n"
-//                  << AxisLabel0.toStdString()
-//                  << "\n"
-//                  << AxisLabel1.toStdString()
-//                  << "\n"
-//                  << std::endl;
-
-
-        std::cout << "----- File: " << thisFile.filename << " ----- " << std::endl;
         std::cout << "Data Rank: " << DataRank << "; Expected: " << expectedDataRank << std::endl;
         std::cout << "Alloc Size: " << AllocSize << "; Expected: " << expectedAllocSize
                   << std::endl;
@@ -173,35 +120,28 @@ bool CsvImportAssistantUseCases::runTest()
         std::cout << "Axis Label 1: " << AxisLabel1.toStdString()
                   << "; Expected: " << expectedAxisLabel1.toStdString() << std::endl;
 
+        if (DataRank != expectedDataRank)
+            return false;
 
-    std::cout << "TEST PASSED!" << std::endl;
-                if (DataRank != expectedDataRank)
-                    return false;
+        if (AllocSize != expectedAllocSize)
+            return false;
 
-    std::cout << "TEST PASSED!" << std::endl;
-                if (AllocSize != expectedAllocSize)
-                    return false;
-    std::cout << "TEST PASSED!" << std::endl;
+        if (CoordinateValues != expectedCoordVals)
+            return false;
 
-                if (CoordinateValues != expectedCoordVals)
-                    return false;
+        if (!areVectorsEqual(IntensityValues, expectedIntensVals))
+            return false;
 
-    std::cout << "TEST PASSED!" << std::endl;
-                if (!areVectorsEqual(IntensityValues, expectedIntensVals))
-                    return false;
+        if (UnitsLabel != expectedUnitsLabel)
+            return false;
 
-    std::cout << "TEST PASSED!" << std::endl;
-                if (UnitsLabel != expectedUnitsLabel)
-                    return false;
+        if (AxisLabel0 != expectedAxisLabel0)
+            return false;
 
-    std::cout << "TEST PASSED!" << std::endl;
-                if (AxisLabel0 != expectedAxisLabel0)
-                    return false;
+        if (AxisLabel1 != expectedAxisLabel1)
+            return false;
 
-    std::cout << "TEST PASSED!" << std::endl;
-                if (AxisLabel1 != expectedAxisLabel1)
-                    return false;
+        std::cout << "---------------------------------------------" << std::endl;
     }
-    std::cout << "TEST PASSED!" << std::endl;
-    return testPassed;
+    return true;
 }
--- Tests/Functional/GUI/GUISpecial/CsvImportAssistantUseCases.h
+++ Tests/Functional/GUI/GUISpecial/CsvImportAssistantUseCases.h
@@ -2,8 +2,8 @@
 //
 //  BornAgain: simulate and fit scattering at grazing incidence
 //
-//! @file      Tests/Functional/GUI/GUIPerformanceTest/GUIPerformanceTest.h
-//! @brief     Defines GUI performance functional test.
+//! @file      Tests/Functional/GUI/GUISpecial/GUIUseCases.cpp
+//! @brief     Defines Import Assistant functional test.
 //!
 //! @homepage  http://www.bornagainproject.org
 //! @license   GNU General Public License v3 or higher (see COPYING)
@@ -15,9 +15,9 @@
 #ifndef CSVIMPORTASSISTANTUSECASES_H
 #define CSVIMPORTASSISTANTUSECASES_H
 
+#include "AxisNames.h"
 #include "IFunctionalTest.h"
 #include "SpecularDataItem.h"
-#include "AxisNames.h"
 #include <QString>
 #include <memory>
 
@@ -25,8 +25,139 @@
 class CsvImportAssistantUseCases : public IFunctionalTest
 {
 public:
-    CsvImportAssistantUseCases();
-    ~CsvImportAssistantUseCases();
+    CsvImportAssistantUseCases()
+    {
+        m_dataFileParameters = {
+                //{"unqualified_filename",
+                //firstRow,
+                //lastRow,
+                //coordinateColumn,
+                //intensityColumn,
+                //expectedCoords,
+                //expectedIntensities},
+
+                {"probeA_merged.txt",
+                 m_firstRow,
+                 m_lastRow,
+                 m_coordinateColumn,
+                 m_intensityColumn,
+                 m_expCoords,
+                 m_expIntens},
+
+                {"mg6c_Merged.txt",
+                 m_firstRow,
+                 m_lastRow,
+                 m_coordinateColumn,
+                 m_intensityColumn,
+                 m_expCoords,
+                 m_expIntens},
+
+                {"raw_data_MG-6a-XRR-Standard_0-1_5deg-step1-2.csv",
+                 m_firstRow,
+                 m_lastRow,
+                 m_coordinateColumn,
+                 m_intensityColumn,
+                 m_expCoords,
+                 m_expIntens},
+
+                {"NonExistentFile",
+                 m_firstRow,
+                 m_lastRow,
+                 m_coordinateColumn,
+                 m_intensityColumn,
+                 m_expCoords,
+                 m_expIntens},
+
+                {"Cut_2.dat",
+                 m_firstRow,
+                 m_lastRow,
+                 m_coordinateColumn + 2,
+                 m_intensityColumn,
+                 m_expCoords,
+                 m_expIntens},
+
+                {"Cut_1.dat",
+                 m_firstRow,
+                 m_lastRow,
+                 m_coordinateColumn + 2,
+                 m_intensityColumn,
+                 m_expCoords,
+                 m_expIntens},
+
+                {"p12537_00000125.dat",
+                 200 + m_firstRow,
+                 200 + m_lastRow,
+                 m_coordinateColumn,
+                 m_intensityColumn + 5,
+                 m_expCoords,
+                 m_expIntens},
+
+                {"p12537_00000128.dat",
+                 200 + m_firstRow,
+                 200 + m_lastRow,
+                 m_coordinateColumn,
+                 m_intensityColumn + 5,
+                 m_expCoords,
+                 m_expIntens},
+
+                {"p15145_00005044.dat",
+                 200 + m_firstRow,
+                 200 + m_lastRow,
+                 m_coordinateColumn,
+                 m_intensityColumn + 9,
+                 m_expCoords,
+                 m_expIntens},
+
+                {"p15145_00005023.dat",
+                 200 + m_firstRow,
+                 200 + m_lastRow,
+                 m_coordinateColumn,
+                 m_intensityColumn + 13,
+                 m_expCoords,
+                 m_expIntens},
+
+                {"NR_System2_D2O_T20.DAT",
+                 m_firstRow,
+                 m_lastRow,
+                 m_coordinateColumn,
+                 m_intensityColumn + 1,
+                 m_expCoords,
+                 m_expIntens},
+
+                {"R_d2o.txt",
+                 m_firstRow,
+                 m_lastRow,
+                 m_coordinateColumn,
+                 m_intensityColumn,
+                 m_expCoords,
+                 m_expIntens},
+
+                {"P_ME_305_D2O_15C_01.DAT",
+                 m_firstRow,
+                 m_lastRow,
+                 m_coordinateColumn + 2,
+                 m_intensityColumn - 1,
+                 m_expCoords,
+                 m_expIntens},
+
+                {"P_ME_3026_D2O_60C_05.DAT",
+                 m_firstRow,
+                 m_lastRow,
+                 m_coordinateColumn + 2,
+                 m_intensityColumn - 1,
+                 m_expCoords,
+                 m_expIntens},
+
+                {"b_104.dat",
+                 m_firstRow,
+                 m_lastRow,
+                 m_coordinateColumn,
+                 m_intensityColumn,
+                 m_expCoords,
+                 m_expIntens},
+        };
+    }
+    ~CsvImportAssistantUseCases() {}
 
 protected:
     struct datafile {
@@ -45,12 +176,11 @@ protected:
     int m_lastRow = 60;
     int m_coordinateColumn = 1;
     int m_intensityColumn = 2;
-    std::vector<double> m_expCoords{10.,20.,30.,40.,50.,60.,70.,80.,90.,100.};
-    std::vector<double> m_expIntens{1.,.9,.8,.7,.6,.5,.4,.3,.2,.1};
+    std::vector<double> m_expCoords{10., 20., 30., 40., 50., 60., 70., 80., 90., 100.};
+    std::vector<double> m_expIntens{1., .9, .8, .7, .6, .5, .4, .3, .2, .1};
     double m_coordinateMultiplier = 10.0;
     double m_intensityMultiplier = 0.1;
     AxesUnits m_units = AxesUnits::RADIANS;
-
 
 private:
 };
