f2994db576c7df8463b99c77abee92434c5bc32b
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2019-06-07 11:39:34 +0200

    Extract the heavy reflectometry computation to an inline function

--- Core/Computation/SpecularComputation.cpp
+++ Core/Computation/SpecularComputation.cpp
@@ -20,6 +20,10 @@
 #include <complex>
 #define TINY 1e-30
 
+namespace
+{
+
+using SpecularElementIter = std::vector<SpecularSimulationElement>::iterator;
 inline void matmul(std::complex<double> a[2][2], std::complex<double> b[2][2],
                    std::complex<double> c[2][2])
 {
@@ -29,6 +33,141 @@ inline void matmul(std::complex<double> a[2][2], std::complex<double> b[2][2],
     c[1][1] = a[1][0] * b[0][1] + a[1][1] * b[1][1];
 }
 
+inline void fillQvals(const SpecularElementIter it_begin, const SpecularElementIter it_end,
+                      std::vector<double_t>& qvals)
+{
+    for (auto it = it_begin; it != it_end; ++it) {
+        qvals.push_back(-it->kz());
+    }
+}
+
+inline void extractSldThicknessRoughnessAndQvalsFromSample(const MultiLayer& multi_layer,
+                                                           const SimulationOptions& sim_options,
+                                                           std::vector<complex_t>& slds,
+                                                           std::vector<double_t>& thicknesses,
+                                                           std::vector<double_t>& roughnesses)
+{
+
+    const std::unique_ptr<MultiLayer> avr_sample =
+        IComputationUtils::CreateAveragedMultilayer(multi_layer, sim_options);
+    for (size_t i = 0; i < avr_sample->numberOfLayers(); i++) {
+        double th = avr_sample->layerThickness(i);
+
+        complex_t sl = avr_sample->layerMaterial(i).SLD(1.0);
+        double rg = 0.0;
+
+        slds.push_back(sl);
+        thicknesses.push_back(th);
+        roughnesses.push_back(rg);
+    }
+}
+
+inline void heavyComputation(std::vector<double_t>& reflectometry,
+                             const std::vector<complex_t>& slds,
+                             const std::vector<double_t>& thicknesses,
+                             const std::vector<double_t>& roughnesses,
+                             const std::vector<double_t> qvals)
+{
+
+
+    using namespace std;
+
+    double scale, bkg;
+    double num = 0, den = 0, answer = 0;
+    size_t npoints = qvals.size();
+
+    complex<double> super;
+    complex<double> sub;
+    complex<double> oneC = complex<double>(1., 0.);
+    complex<double> MRtotal[2][2];
+    complex<double> MI[2][2];
+    complex<double> temp2[2][2];
+    complex<double> qq2;
+    complex<double>* SLD = nullptr;
+    complex<double>* thickness = nullptr;
+    double* rough_sqr = nullptr;
+
+    size_t nlayers = size_t(thicknesses.size());
+
+    scale = 1.0;
+    bkg = 0.0;
+    sub = complex<double>(2.07e-06, 0.0 + TINY);
+    super = complex<double>(0.00, 0.0);
+
+    try {
+        SLD = new complex<double>[nlayers + 2];
+        thickness = new complex<double>[nlayers];
+        rough_sqr = new double[nlayers + 1];
+    } catch (...) {
+        goto done;
+    }
+    // fill out all the SLD's for all the layers
+    for (int ii = 1; ii < int(nlayers + 1); ii += 1) {
+        SLD[ii] = 4 * M_PI * (complex<double>(slds[size_t(ii - 1)]) + TINY) - super;
+        thickness[ii - 1] = complex<double>(0, thicknesses[size_t(ii - 1)]);
+        rough_sqr[ii - 1] = -2 * roughnesses[size_t(ii - 1)] * roughnesses[size_t(ii - 1)];
+    }
+
+    SLD[0] = complex<double>(0, 0);
+    SLD[nlayers + 1] = 4 * M_PI * (sub - super);
+    rough_sqr[nlayers] = 0.0;
+
+    for (size_t j = 0; j < npoints; j++) {
+        complex<double> beta, rj;
+        complex<double> kn, kn_next;
+
+        qq2 = complex<double>(qvals[j] * qvals[j] / 4, 0);
+
+        // now calculate reflectivities and wavevectors
+        kn = std::sqrt(qq2);
+        for (int ii = 0; ii < int(nlayers + 1); ii++) {
+            // wavevector in the layer
+            kn_next = std::sqrt(qq2 - SLD[ii + 1]);
+
+            // reflectance of the interface
+            rj = (kn - kn_next) / (kn + kn_next) * std::exp(kn * kn_next * rough_sqr[ii]);
+
+            if (!ii) {
+                // characteristic matrix for first interface
+                MRtotal[0][0] = oneC;
+                MRtotal[0][1] = rj;
+                MRtotal[1][1] = oneC;
+                MRtotal[1][0] = rj;
+            } else {
+                // work out the beta for the layer
+                beta = std::exp(kn * thickness[ii - 1]);
+                // this is the characteristic matrix of a layer
+                MI[0][0] = beta;
+                MI[0][1] = rj * beta;
+                MI[1][1] = oneC / beta;
+                MI[1][0] = rj * MI[1][1];
+
+                // multiply MRtotal, MI to get the updated total matrix.
+                memcpy(temp2, MRtotal, sizeof(MRtotal));
+                matmul(temp2, MI, MRtotal);
+            }
+            kn = kn_next;
+        }
+
+        num = std::norm(MRtotal[1][0]);
+        den = std::norm(MRtotal[0][0]);
+        answer = (num / den);
+        answer = (answer * scale) + bkg;
+
+        reflectometry[j] = answer;
+    }
+
+done:
+    if (SLD)
+        delete[] SLD;
+    if (thickness)
+        delete[] thickness;
+    if (rough_sqr)
+        delete[] rough_sqr;
+
+
+}
+}
 
 static_assert(std::is_copy_constructible<SpecularComputation>::value == false,
               "SpecularComputation should not be copy constructible");
@@ -39,9 +178,9 @@ SpecularComputation::SpecularComputation(const MultiLayer& multilayer,
                                          const SimulationOptions& options,
                                          ProgressHandler& progress, SpecularElementIter begin_it,
                                          SpecularElementIter end_it)
-    : IComputation(options, progress, multilayer)
-    , m_begin_it(begin_it), m_end_it(end_it)
-{}
+    : IComputation(options, progress, multilayer), m_begin_it(begin_it), m_end_it(end_it)
+{
+}
 
 SpecularComputation::~SpecularComputation() = default;
 
@@ -51,133 +190,22 @@ void SpecularComputation::runProtected()
         return;
 
     m_computation_term.setProgressHandler(mp_progress);
-    const std::unique_ptr<MultiLayer> avr_sample =
-                    IComputationUtils::CreateAveragedMultilayer(*mP_multi_layer, m_sim_options);
-    double doubleVal(3.14159265);
+
     std::vector<complex_t> slds;
     std::vector<double_t> thicknesses;
     std::vector<double_t> roughnesses;
-    for(size_t i = 0; i < avr_sample->numberOfLayers(); i++){
-        double th = avr_sample->layerThickness(i);
-        complex_t sl = avr_sample->layerMaterial(i).materialData();
-        double rg = 0.0;
-
-        slds.push_back(sl);
-        thicknesses.push_back(th);
-        roughnesses.push_back(rg);
-    }
+    extractSldThicknessRoughnessAndQvalsFromSample(*mP_multi_layer, m_sim_options, slds,
+                                                   thicknesses, roughnesses);
 
     std::vector<double_t> qvals;
-    for (auto it = m_begin_it; it != m_end_it; ++it){
-        qvals.push_back(it->kz());
-    }
-    auto npoints = qvals.size();
+    fillQvals(m_begin_it, m_end_it, qvals);
+
+    size_t npoints = qvals.size();
     std::vector<double> reflectometry(npoints);
-
-    {
-        {
-            using namespace std;
-
-            double scale, bkg;
-            double num = 0, den = 0, answer = 0;
-
-            complex<double> super;
-            complex<double> sub;
-            complex<double> oneC = complex<double>(1., 0.);
-            complex<double> MRtotal[2][2];
-            complex<double> MI[2][2];
-            complex<double> temp2[2][2];
-            complex<double> qq2;
-            complex<double>* SLD = nullptr;
-            complex<double>* thickness = nullptr;
-            double* rough_sqr = nullptr;
-
-            size_t nlayers = size_t(thicknesses.size());
-
-            scale = 1.0;
-            bkg = 0.0;
-            sub = complex<double>(2.07, 0.0 + TINY);
-            super = complex<double>(0.00, 0.0);
-
-            try {
-                SLD = new complex<double>[nlayers + 2];
-                thickness = new complex<double>[nlayers];
-                rough_sqr = new double[nlayers + 1];
-            } catch (...) {
-                goto done;
-            }
-            // fill out all the SLD's for all the layers
-            for (int ii = 1; ii < int(nlayers + 1); ii += 1) {
-                SLD[ii] = 4e-6 * M_PI * (complex<double>(slds[size_t(ii - 1)]) + TINY) - super;
-                thickness[ii - 1] = complex<double>(0, thicknesses[size_t(ii - 1)]);
-                rough_sqr[ii - 1] = -2 * roughnesses[size_t(ii - 1)] * roughnesses[size_t(ii - 1)];
-            }
-
-            SLD[0] = complex<double>(0, 0);
-            SLD[nlayers + 1] = 4e-6 * M_PI * (sub - super);
-            rough_sqr[nlayers] = 0.0;
-
-            for (size_t j = 0; j < npoints; j++) {
-                complex<double> beta, rj;
-                complex<double> kn, kn_next;
-
-                qq2 = complex<double>(qvals[j] * qvals[j] / 4, 0);
-
-                // now calculate reflectivities and wavevectors
-                kn = std::sqrt(qq2);
-                for (int ii = 0; ii < int(nlayers + 1); ii++) {
-                    // wavevector in the layer
-                    kn_next = std::sqrt(qq2 - SLD[ii + 1]);
-
-                    // reflectance of the interface
-                    rj = (kn - kn_next) / (kn + kn_next) * std::exp(kn * kn_next * rough_sqr[ii]);
-
-                    if (!ii) {
-                        // characteristic matrix for first interface
-                        MRtotal[0][0] = oneC;
-                        MRtotal[0][1] = rj;
-                        MRtotal[1][1] = oneC;
-                        MRtotal[1][0] = rj;
-                    } else {
-                        // work out the beta for the layer
-                        beta = std::exp(kn * thickness[ii - 1]);
-                        // this is the characteristic matrix of a layer
-                        MI[0][0] = beta;
-                        MI[0][1] = rj * beta;
-                        MI[1][1] = oneC / beta;
-                        MI[1][0] = rj * MI[1][1];
-
-                        // multiply MRtotal, MI to get the updated total matrix.
-                        memcpy(temp2, MRtotal, sizeof(MRtotal));
-                        matmul(temp2, MI, MRtotal);
-                    }
-                    kn = kn_next;
-                }
-
-                num = std::norm(MRtotal[1][0]);
-                den = std::norm(MRtotal[0][0]);
-                answer = (num / den);
-                answer = (answer * scale) + bkg;
-
-                reflectometry[j] = answer;
-            }
-
-done:
-            if (SLD)
-                delete[] SLD;
-            if (thickness)
-                delete[] thickness;
-            if (rough_sqr)
-                delete[] rough_sqr;
-
-            // return reflectometry;
-        }
-
-    }
-
+    heavyComputation(reflectometry, slds, thicknesses, roughnesses, qvals);
 
     size_t count = 0;
-    for (auto it = m_begin_it; it != m_end_it; ++it){
+    for (auto it = m_begin_it; it != m_end_it; ++it) {
         it->setIntensity(reflectometry[count++]);
     }
 }
